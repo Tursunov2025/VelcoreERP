262c000204c010ebef7439a8d2e413967d77d494
karimovbotirjon665-lab <karimovbotirjon665@gmail.com>
2026-05-23 15:31:05 +0500

    sqlite production fixed

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg2://postgres:0102@127.0.0.1:5433/postgres"
+DATABASE_URL = "sqlite:///./azmus.db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
