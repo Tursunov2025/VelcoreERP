ad9f46dcdfea4d74bf16c421f64910cbb95826f4
karimovbotirjon665-lab <karimovbotirjon665@gmail.com>
2026-05-23 17:31:55 +0500

    new database created

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./azmus.db"
+DATABASE_URL = "sqlite:///./azmus_new.db"
 
 engine = create_engine(
     DATABASE_URL,
